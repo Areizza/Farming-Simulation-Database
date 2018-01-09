086af8fb70161cced221f3689153f9c5d2586eba
Priscilla <35178358+Areizza@users.noreply.github.com>
2018-01-09 14:51:33 -0500

    Create loginCheck.php
    Username: Aldag
    Password: potato

---
+++ loginCheck.php
@@ -0,0 +1,34 @@
+<?php
+	session_start();
+	
+	if (isset($_SESSION["session_flag"])) {
+			if($_SESSION["session_flag"] == "valid") {
+				print_menu();
+			} else {
+				echo "Invalid session!";
+				echo "<a href=\"login.html\">Click here </a> to go back to the login page.";
+			}
+	} else if( $_POST["user"] || $_POST["password"] ) {
+		if ($_POST["user"] == "Aldag" && $_POST["pass"] == "potato" ) {
+			$_SESSION["session_flag"] = "valid";
+			print_menu();
+			
+		} else {
+			echo "Incorrect login or password. <br />";			
+			echo "<a href=\"login.html\">Click here </a> to go back to the login page.";
+		}
+	  
+	} else {
+		echo "No login or password entered! <br />";
+		echo "<a href=\"login.html\">Click here </a> to go back to the login page.";
+	}
+	
+	function print_menu() {
+		echo "You logged in successfully! <br />";
+		echo "<br />";			
+		echo "Welcome " .$_POST["user"]. "<br />";
+		echo "<br />";	
+		echo "<a href=\"main.html\">Click here</a> to view menu. <br />";
+		echo "<a href=\"logout.php\">Click here</a> to logout. <br />";
+	}
+?>
